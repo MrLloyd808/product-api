72c485253c452b2b723c90007710329c77c887b7
LIL-MANIAC-001 <127343644+MrLloyd808@users.noreply.github.com>
2025-11-09 21:32:19 +0200

    added cors

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require("express")
 require("dotenv").config()
 const connect = require("./DB/connectdb")
 const path = require("path")
+const cors = require("cors")
 const ProductRouter = require("./ROUTER/productRoute")
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use("/products", ProductRouter)
 app.use(express.static(path.join(__dirname, "uploads")))
 
 try {
     connect()
     .then(() => {
-        app.listen(process.env.PORT, () => {
+        app.listen(process.env.PORT || 8000, () => {
         console.log("server is listening")
 })
 })
